ae455a64bd7ed9717f049f17f3d60e3cb20cc130
Andres <A01781321@tec.mx>
2024-03-31 21:08:26 -0600

    actualizacion del juego ataque

--- Time Clash Chronicles/Assets/Scripts/CardController.cs
+++ Time Clash Chronicles/Assets/Scripts/CardController.cs
@@ -3,100 +3,121 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using TMPro; // Add this line to include TextMeshPro namespace
+using TMPro;
 
 public class CardController : MonoBehaviour, IPointerClickHandler
 {
     private bool isSelected = false;
-    public Image selectionHighlight;
     private CardController selectedCardController;
-    private ArenaManager arenaManager; // Reference to the ArenaManager
+    private ArenaManager arenaManager;
 
-    // Add a field to reference the TextMeshPro Text component
+    public Image selectionHighlight;
+    public TextMeshProUGUI attackText;
     public TextMeshProUGUI healthText;
-
-    // Add a field to reference the Card scriptable object
     public Card cardData;
 
-    void Start()
+    private int initialHealth; // Store initial health value
+
+    
+void Start()
+{
+    selectionHighlight.enabled = false;
+    arenaManager = FindObjectOfType<ArenaManager>();
+
+    if (cardData != null)
     {
-        selectionHighlight.enabled = false;
-        arenaManager = FindObjectOfType<ArenaManager>(); // Find the ArenaManager in the scene
+        initialHealth = cardData.health;
+        ResetHealth();
     }
+    else
+    {
+        Debug.LogError("Card data is not assigned to CardController: " + gameObject.name);
+    }
+}
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        ToggleSelection();
-
-        if (isSelected)
+        if (!isSelected)
         {
-            if (selectedCardController != null)
-            {
-                // Check if the selected card is in the enemy player's field
-                if (arenaManager.enemyCards.Contains(selectedCardController))
-                {
-                    // Attack the selected card
-                    Debug.Log(gameObject.name + " attacks " + selectedCardController.gameObject.name);
-
-                    // Reduce the health of the attacked card
-                    selectedCardController.TakeDamage(cardData.attack);
-
-                    // Update health text
-                    UpdateHealthText();
-                }
-                else
-                {
-                    Debug.Log("Cannot attack this card.");
-                }
-            }
-            else
-            {
-                Debug.Log("No card selected to attack.");
-            }
-        }
-    }
-
-    void ToggleSelection()
-    {
-        isSelected = !isSelected;
-        selectionHighlight.enabled = isSelected;
-
-        // Set the selected card controller if the card is selected
-        if (isSelected)
-        {
-            // Set the selected card controller from the clicked card
-            selectedCardController = this;
+            SelectCard();
         }
         else
         {
-            // Clear the selected card controller if the card is deselected
-            selectedCardController = null;
+            Attack();
         }
     }
 
-    // Method to handle taking damage
-    public void TakeDamage(int damage)
+    void SelectCard()
     {
-        cardData.health -= damage;
-        Debug.Log(gameObject.name + " takes " + damage + " damage. Remaining health: " + cardData.health);
+        isSelected = true;
+        selectedCardController = this;
+        selectionHighlight.enabled = true;
 
-        // Check if the card has been defeated
-        if (cardData.health <= 0)
+        if (attackText != null && healthText != null)
         {
-            Debug.Log(gameObject.name + " has been defeated!");
-            Destroy(gameObject);
+            attackText.text = "Attack: " + cardData.attack.ToString();
+            healthText.text = "Health: " + cardData.health.ToString();
         }
-
-        // Update health text
-        UpdateHealthText();
     }
 
-    // Method to update the health text
-    private void UpdateHealthText()
+    void Attack()
+    {
+        // Check if the selected card belongs to the enemy's side
+        bool isEnemyCard = arenaManager.enemyCards.Contains(selectedCardController);
+
+        if (isEnemyCard)
+        {
+            int prevHealth = selectedCardController.cardData.health;
+            int currentHealth = prevHealth - cardData.attack;
+
+            Debug.Log("Attack: " + cardData.attack + ", Enemy Health: " + prevHealth + " -> " + currentHealth);
+
+            selectedCardController.cardData.health = currentHealth;
+            selectedCardController.UpdateHealthText();
+
+            if (currentHealth <= 0)
+            {
+                Destroy(selectedCardController.gameObject); // Destroy the card if its health reaches 0
+            }
+        }
+        else
+        {
+            Debug.Log("Please select an enemy card to attack.");
+        }
+
+        DeselectCard();
+    }
+
+    void DeselectCard()
+    {
+        isSelected = false;
+        selectedCardController = null;
+        selectionHighlight.enabled = false;
+
+        if (attackText != null && healthText != null)
+        {
+            attackText.text = "";
+            healthText.text = "";
+        }
+    }
+
+    public void UpdateHealthText()
     {
         if (healthText != null)
         {
             healthText.text = "Health: " + cardData.health.ToString();
         }
+    }
+
+    void ResetHealth()
+    {
+        cardData.health = initialHealth; // Reset card's health to its initial value
+        UpdateHealthText();
+    }
+
+    void OnDisable()
+    {
+        // Reset health when the scene is stopped in the Unity Editor
+        ResetHealth();
     }
 }
