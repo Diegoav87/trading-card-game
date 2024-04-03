754e06000b81f6c60145d994cfcad13511498bef
IsaacShakalo <ishakalopaz@gmail.com>
2024-04-03 13:24:04 -0600

    Terminada la api

--- Game_API/app.js
+++ Game_API/app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import { check, validationResult } from "express-validator";
 import express from "express";
 import mysql from "mysql2/promise"
 
@@ -13,7 +14,7 @@ async function connectToDB() {
   return await mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "diego",
+    password: "17June22!",
     database: "time_clash_chronicles",
   });
 }
@@ -51,6 +52,10 @@ app.get("/api/cards/:id", async (request, response) => {
 
     const [results, fields] = await connection.execute("SELECT * FROM card WHERE card_id = ?", [request.params.id]);
 
+    if (results.length === 0) {
+      return response.status(404).json({ error: "No se encontró ninguna carta con el ID proporcionado" });
+    }
+
     response.status(200).json(results);
   }
   catch (error) {
@@ -71,19 +76,34 @@ app.post("/api/players", async (request, response) => {
   let connection = null;
 
   try {
-    connection = await connectToDB();
+  connection = await connectToDB();
 
-    const playerData = request.body;
+  const data = request.body;
+  console.log(data.name)
 
-    const [results, fields] = await connection.execute(
-      "INSERT INTO player (name) VALUES (?)",
-      [playerData.name]
-    );
+  await check(data.name).custom((value) => {
+    if (!value || value === '') {
+      
+    }
+    return true;
+  });
 
-    console.log(`${results.affectedRows} rows affected`);
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    return response.status(400).json({ errors: errors.array() });
+  }
 
-    response.status(200).json({ message: "Jugador creado exitosamente" });
-  } catch (error) {
+  const playerData = request.body;
+
+  const [results, fields] = await connection.execute(
+    "INSERT INTO player (name) VALUES (?)",
+    [playerData.name]
+  );
+
+  console.log(`${results.affectedRows} rows affected`);
+
+  response.status(200).json({ message: "Jugador creado exitosamente" });
+}  catch (error) {
     console.error(error);
     response.status(500).json({ error: "Error al crear el jugador" });
   } finally {
@@ -101,6 +121,10 @@ app.get("/api/players/:id", async (request, response) => {
     connection = await connectToDB();
 
     const [results, fields] = await connection.execute("SELECT * FROM player WHERE player_id = ?", [request.params.id]);
+
+    if (results.length === 0) {
+      return response.status(404).json({ error: "No se encontró ningun jugador con el ID proporcionado" });
+    }
 
     response.status(200).json(results);
   }
@@ -127,6 +151,10 @@ app.get("/api/players/deck/:id", async (request, response) => {
 
     const [results, fields] = await connection.execute("SELECT * FROM deck WHERE deck_id = ?", [request.params.id]);
 
+    if (results.length === 0) {
+      return response.status(404).json({ error: "No se encontró ningun deck con el ID proporcionado" });
+    }
+
     response.status(200).json(results);
   }
   catch (error) {
@@ -149,17 +177,52 @@ app.post("/api/games", async (request, response) => {
   try {
     connection = await connectToDB();
 
-    const data = request.body instanceof Array ? request.body : [request.body];
+    const data = request.body;
 
-    for (const card of data) {
+    console.log("Duration value:", data.duration);
 
-      const [results, fields] = await connection.execute(
-        "INSERT INTO game (player1_id, player2_id, winner_id, arena_id, duration) VALUES (?,?,?,?,?)",
-        [card.player1_id, card.player2_id, card.winner_id, card.arena_id, card.duration]
-      );
-      console.log(`${results.affectedRows} Game affected`);
-      console.log(results);
+    await Promise.all([
+      check(data.player1_id).isInt({ min: 1 }).withMessage("El ID del jugador 1 debe ser un número entero positivo"),
+      check(data.player2_id).isInt({ min: 1 }).withMessage("El ID del jugador 2 debe ser un número entero positivo"),
+      check(data.winner_id).isInt({ min: 1 }).withMessage("El ID del ganador debe ser un número entero positivo"),
+      check(data.arena_id).isInt({ min: 1 }).withMessage("El ID de la arena debe ser un número entero positivo"),
+      check(data.duration).isInt({ min: 1, max: 3600 }).withMessage("La duración del juego debe ser un número entero positivo menor o igual a 3600 segundos")
+    ]).then(() => {
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        throw errors.array();
+      }
+    });
+
+    
+
+    // Verificar la existencia de los jugadores y la arena en la base de datos
+    const [player1Result] = await connection.execute("SELECT 1 FROM player WHERE player_id = ?", [data.player1_id]);
+    if (player1Result.length === 0) {
+      return response.status(400).json({ error: `El jugador con ID ${data.player1_id} no existe en la base de datos` });
     }
+
+    const [player2Result] = await connection.execute("SELECT 1 FROM player WHERE player_id = ?", [data.player2_id]);
+    if (player2Result.length === 0) {
+      return response.status(400).json({ error: `El jugador con ID ${data.player2_id} no existe en la base de datos` });
+    }
+
+    const [winnerResult] = await connection.execute("SELECT 1 FROM player WHERE player_id = ?", [data.winner_id]);
+    if (winnerResult.length === 0) {
+      return response.status(400).json({ error: `El jugador con ID ${data.winner_id} no existe en la base de datos` });
+    }
+
+    const [arenaResult] = await connection.execute("SELECT 1 FROM arena WHERE arena_id = ?", [data.arena_id]);
+    if (arenaResult.length === 0) {
+      return response.status(400).json({ error: `La arena con ID ${data.arena_id} no existe en la base de datos` });
+    }
+
+    const [results, fields] = await connection.execute(
+      "INSERT INTO game (player1_id, player2_id, winner_id, arena_id, duration) VALUES (?,?,?,?,?)",
+      [data.player1_id, data.player2_id, data.winner_id, data.arena_id, data.duration]
+    );
+    console.log(`${results.affectedRows} Game affected`);
+    console.log(results);
 
     response.status(200).json({ message: "Game added successfully" });
   }
@@ -171,46 +234,124 @@ app.post("/api/games", async (request, response) => {
   finally {
     if (connection !== null) {
       connection.end();
-      console.log("Connection closed succesfully!");
+      console.log("Connection closed successfully!");
     }
   }
 });
 
 
-app.post("/api/turns", async (request, response) => {
+app.get("/api/games/:gameId", async (request, response) => {
+  const gameId = request.params.gameId;
   let connection = null;
 
   try {
     connection = await connectToDB();
 
-    const turnData = request.body;
-
-    // Assuming your 'turn' table has fields like 'player_id', 'game_id', 'turn_number', etc.
     const [results, fields] = await connection.execute(
-      "INSERT INTO turn (player_id, game_id, turn_number, phase) VALUES (?,?,?,?)",
-      [turnData.player_id, turnData.game_id, turnData.turn_number, turnData.phase]
+      "SELECT * FROM game WHERE game_id = ?",
+      [gameId]
     );
 
-    console.log(`${results.affectedRows} rows affected`);
+    if (results.length === 0) {
+      return response.status(404).json({ error: "No se encontró ningun juego con el ID proporcionado" });
+    }
 
-    response.status(200).json({ message: "Turno creado exitosamente" });
+    if (results.length === 0) {
+      response.status(404).json({ error: "Game not found" });
+    } else {
+      response.status(200).json(results[0]);
+    }
   } catch (error) {
+    response.status(500).json({ error: "Internal Server Error" });
     console.error(error);
-    response.status(500).json({ error: "Error al crear el turno" });
   } finally {
     if (connection !== null) {
       connection.end();
-      console.log("Conexión cerrada exitosamente");
+      console.log("Connection closed successfully!");
     }
   }
 });
 
 
 
+app.get("/api/deck", async (request, response) => {
+  let connection = null;
 
+  try {
+    connection = await connectToDB();
 
+    const [results, fields] = await connection.execute("SELECT * FROM deck");
 
+    response.status(200).json(results);
+  } catch (error) {
+    response.status(500);
+    response.json(error);
+    console.log(error);
+  } finally {
+    if (connection !== null) {
+      connection.end();
+      console.log("Connection closed successfully!");
+    }
+  }
+});
 
+app.get("/api/deck/:id", async (request, response) => {
+  let connection = null;
+
+  try {
+    connection = await connectToDB();
+
+    const [results, fields] = await connection.execute("SELECT * FROM deck WHERE deck_id = ?", [request.params.id]);
+
+    if (results.length === 0) {
+      return response.status(404).json({ error: "No se encontró ningun deck con el ID proporcionado" });
+    }
+
+    response.status(200).json(results);
+  }
+  catch (error) {
+    response.status(500);
+    response.json(error);
+    console.log(error);
+  }
+  finally {
+
+    if (connection !== null) {
+      connection.end();
+      console.log("Connection closed succesfully!");
+    }
+  }
+});
+
+app.get("/api/decks/:deckId/cards", async (request, response) => {
+  const deckId = request.params.deckId;
+  let connection = null;
+
+  try {
+    connection = await connectToDB();
+
+    const [results, fields] = await connection.execute(
+      `SELECT c.* FROM card c
+       INNER JOIN deck_card dc ON c.card_id = dc.card_id
+       WHERE dc.deck_id = ?`,
+      [deckId]
+    );
+
+    if (results.length === 0) {
+      return response.status(404).json({ error: "No se encontró ningun deck con el ID proporcionado" });
+    }
+
+    response.status(200).json(results);
+  } catch (error) {
+    response.status(500).json({ error: "Internal Server Error" });
+    console.error(error);
+  } finally {
+    if (connection !== null) {
+      connection.end();
+      console.log("Connection closed successfully!");
+    }
+  }
+});
 
 app.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
